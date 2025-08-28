4ccfefe94130730e4d5fa648ca8ef1ea65bbafce
Jahnavi-114 <challajahnavi01@gmail.com>
2025-08-28 22:11:13 +0530

    Add files via upload

---
+++ 28082025-2.sql
@@ -0,0 +1,50 @@
+SHOW DATABASES;
+USE sathyabama;
+create table award_winners(
+id int,
+name varchar(25),
+award_name varchar(30));
+
+insert into award_winners values
+(001,'Karthick','Volleyball winners'),
+(002,'Gopi','Basketball Runners'),
+(003,'Adib','Oscar winners'),
+(004,'Harij','English SpellBee 2nd Runner-up'),
+(005,'Loki','Volleyball winners'),
+(006,'Sakthi','Badminton Nationals runners'),
+(007,'Iqhlas','Debug winners'),
+(008,'AK','Basketball runners'),
+(009,'Kamal','Hackathon runners'),
+(010,'Barath','Hackathon winners');
+
+select * from award_winners;
+
+create table student(
+id int,
+name varchar(25));
+
+insert into student values
+(005,'Loki'),
+(1001,'Srihari'),
+(1003,'Dinesh'),
+(001,'Karthick'),
+(1005,'Tharun'),
+(1006,'Vamsi'),
+(1007,'Monish'),
+(1008,'Rohith'),
+(1009,'Alwyn'),
+(1010,'Akash');
+
+select * from student;
+
+select * from student
+where id=(select id from award_winners where id=5);
+
+select * from student
+where id=(select id from award_winners where name="karthick");
+
+select * from award_winners
+where id=(select id from students where id=1);
+
+select * from award_winners
+where id=(select id from students where name = "loki");
